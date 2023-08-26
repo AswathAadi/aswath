725da8ba09f427df38222320447b18d39f725dc9
Aswathaman M <aswathamanm@sweyainfotech.com>
2023-08-26 13:00:43 +0530

    Code changes for the Event listener to prevent the page reload

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ function SendEmail(){
           console.log('FAILED...', error);
 });
 }
+
+
+document.getElementById("form").addEventListener("submit", function(event) {
+    // Prevent the default form submission behavior (page reload)
+    event.preventDefault();
+    
+    // Perform your form processing here
+    // You can use AJAX to send the form data to the server
+    
+    // Optionally, you can also update the page content without a full reload
+    
+    // Example: Display a success message
+    var successMessage = document.createElement("p");
+    successMessage.textContent = "Form submitted successfully!";
+    document.body.appendChild(successMessage);
+});
