04adc9aca169d56a25bbe0c26f88dbe95ee8933a
Aswathaman M <aswathamanm@sweyainfotech.com>
2023-08-26 12:43:45 +0530

    Code changes for Email.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-const data = { };
-
-function SendValues(e)
-{
-    data[e.name] = e.value;
-}
-
-function SendEmail(){    
-      emailjs.send('service_5qxc8g9', 'template_8pnt5pf', data)
+function SendEmail(){
+    var templateParams = {
+        from_name : document.getElementById("from_name").value,
+        from_email : document.getElementById("from_email").value,
+        from_mobile : document.getElementById("from_mobile").value,
+        from_mobile : document.getElementById("from_mobile").value,
+        from_message : document.getElementById("from_message_data").value
+      };
+    
+      emailjs.send('service_5qxc8g9', 'template_8pnt5pf', templateParams)
         .then(function(response) {
           console.log('SUCCESS!', response.status, response.text);
         }, function(error) {
